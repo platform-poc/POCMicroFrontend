918befcda24fd4f441d34e5bda0061b9aab2477d
pandey4me <pandey4me@gmail.com>
2023-07-18 09:19:37 +0200

    Update and rename .eslintrc to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    // Add your ESLint rules here
+  },
+};
